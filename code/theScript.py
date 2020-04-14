a3b0b28d13ae077f64d48a2dec1a21526b1e80b3
IonianIronist <36776432+IonianIronist@users.noreply.github.com>
2020-04-15 02:33:41 +0300

    Update theScript.py

--- code/theScript.py
+++ code/theScript.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import random
+import convexHull
 import sorters
 import fn
 
@@ -10,5 +10,26 @@ fn.make_points(points)
 print(*points)
 sorters.quick_sort(points, 0, len(points)-1)
 print(*points)
-# plt.scatter([x[0] for x in points], [y[1] for y in points])
-# plt.show()
+hull = convexHull.convex_hull(points)
+
+print("\n\n\n\n\n")
+print(*hull)
+
+colors = []
+
+for x in points:
+    if x in hull:
+        colors.append('red')
+    else:
+        colors.append('blue')
+
+plt.scatter([x[0] for x in points], [y[1] for y in points], s=10, c=colors)
+for i in range(len(hull)):
+    x = [val[0] for val in hull]
+    y = [val[1] for val in hull]
+    x.append(hull[0][0])
+    x.append(hull[-1][0])
+    y.append(hull[0][1])
+    y.append(hull[-1][-1])
+    plt.plot(x, y)
+plt.show()
