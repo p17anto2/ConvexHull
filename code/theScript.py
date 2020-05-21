56af218b3597cf17cec0ae781b0a9be74a67ddd7
IonianIronist <36776432+IonianIronist@users.noreply.github.com>
2020-05-21 15:18:16 +0300

    Update theScript.py

--- code/theScript.py
+++ code/theScript.py
@@ -2,34 +2,53 @@ import matplotlib.pyplot as plt
 import convexHull
 import sorters
 import fn
+import time
 
 n = 100
 points = []
-fn.make_points(points, n)
+times = []
+title = ("Bubble Sort", "Quick Sort", "Insertion Sort", "Merge Sort")
 
-print(*points)
-sorters.quick_sort(points, 0, len(points)-1)
-print(*points)
-hull = convexHull.convex_hull(points)
+for i in range(4):
+    n = 100
+    for j in range(20):
+        # making the points to plot, from 100 to 1000 with step 100
+        fn.make_points(points, n)
+        start = time.time()  # start the timer
+        if i != 3:
+            sorters.choose_sorter(i, points)
+        else:
+            points = sorters.merge_sort(points)
+        hull = convexHull.convex_hull(points)
+        end = time.time()  # end the timer after sorting
+        times.append((n, end-start))
+        if n in (100, 500, 1000):
+            colors = []
 
-print("\n\n\n\n\n")
-print(*hull)
+            for x in points:
+                if x in hull:
+                    colors.append('red')
+                else:
+                    colors.append('blue')
 
-colors = []
+            plt.scatter([x[0] for x in points], [y[1] for y in points], s=10, c=colors)
+            for _ in range(len(hull)):
+                x = [val[0] for val in hull]
+                y = [val[1] for val in hull]
+                x.append(hull[0][0])
+                x.append(hull[-1][0])
+                y.append(hull[0][1])
+                y.append(hull[-1][-1])
+                plt.plot(x, y)
+            the_title = title[i] + " " + str(n) + " Points"
+            plt.title(the_title)
+            plt.show()
+        n += 100
+    print("all the times ")
+    print("No of points, time")
+    print(*times)
+    plt.scatter([x[0] for x in times], [y[1] for y in times], s=10)
+    plt.show()
+    points.clear()
+    times.clear()
 
-for x in points:
-    if x in hull:
-        colors.append('red')
-    else:
-        colors.append('blue')
-
-plt.scatter([x[0] for x in points], [y[1] for y in points], s=10, c=colors)
-for i in range(len(hull)):
-    x = [val[0] for val in hull]
-    y = [val[1] for val in hull]
-    x.append(hull[0][0])
-    x.append(hull[-1][0])
-    y.append(hull[0][1])
-    y.append(hull[-1][-1])
-    plt.plot(x, y)
-plt.show()
